eba69479a0a47cedc53aa94147bf5dff607b8a8d
david georgette <davidgeorgette974@gmail.com>
2025-04-25 11:56:51 +0000

    Small web app wrapper around the Game

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-docstring
+from flask import Flask, render_template
+from longest_word2.game import Game
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    game = Game()
+    return render_template('home.html', grid = game.grid)
